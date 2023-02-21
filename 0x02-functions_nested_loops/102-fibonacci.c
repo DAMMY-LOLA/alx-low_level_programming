ec4a46118a362c3fbe1dd58ffa6261146b4c43ad
akinola damilola <akinoladamilola1604@gmail.com>
2023-02-21 13:17:12 -0800

    push

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,21 +8,19 @@
  */
 int main(void)
 {
-	long int n1, n2, fn, afn;
+    int fib[50];
+    fib[0] = 1;
+    fib[1] = 2;
+    printf("%d, %d, ", fib[0], fib[1]);
 
-	n1 = 1;
-	n2 = 2;
-	fn = afn = 0;
-	while (fn <= 4000000)
-	{
-		fn = n1 + n2;
-		n1 = n2;
-		n2 = fn;
-		if ((n1 % 2) == 0)
-		{
-			afn += n1;
-		}
-	}
-	printf("%ld\n", afn);
+    for (int i = 2; i < 50; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+        printf("%d", fib[i]);
+        if (i != 49) {
+            printf(", ");
+        }
+    }
+
+   	 printf("\n");
 	return (0);
 }
