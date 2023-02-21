ffefdb2be37cc31c6c316b582632720d72a989c0
akinola damilola <akinoladamilola1604@gmail.com>
2023-02-21 10:43:52 -0800

    redoing

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - Prints 9 times table, strating with 0.
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            int result = i * j;
+
+            if (j == 0)
+            {
+                printf("%d", result);
+            }
+            else if (result < 10)
+            {
+                printf(",  %d", result);
+            }
+            else
+            {
+                printf(", %d", result);
+            }
+        }
+        printf("\n");
+    }
+}
