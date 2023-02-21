1fbed1ca7608665a03be06811205a45078886ea8
akinola damilola <akinoladamilola1604@gmail.com>
2023-02-21 14:25:03 -0800

    104-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Prints the add of the even-valued
- * fibonacci numbers.
- *
- * Return: Always 0.
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    int fib[50];
-    fib[0] = 1;
-    fib[1] = 2;
-    printf("%d, %d, ", fib[0], fib[1]);
+	unsigned long int a, b, c, b1, b2, c1, c2;
 
-    for (int i = 2; i < 50; i++) {
-        fib[i] = fib[i-1] + fib[i-2];
-        printf("%d", fib[i]);
-        if (i != 49) {
-            printf(", ");
-        }
-    }
+	b = 1;
+	c = 2;
 
-   	 printf("\n");
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
+	{
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
+	}
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", c1 + (c2 / 1000000000));
+		printf("%lu", c2 % 1000000000);
+		c1 = c1 + b1;
+		b1 = c1 - b1;
+		c2 = c2 + b2;
+		b2 = c2 - b2;
+	}
+	printf("\n");
 	return (0);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - Prints 9 times table, strating with 0.
- *
- * Return: Always 0 (Success)
+ * times_table - prints the 9 times table, startin from 0
  */
 void times_table(void)
 {
-    int i, j;
+	int a;
+	int b;
+	int c;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            int result = i * j;
-
-            if (j == 0)
-            {
-                printf("%d", result);
-            }
-            else if (result < 10)
-            {
-                printf(",  %d", result);
-            }
-            else
-            {
-                printf(", %d", result);
-            }
-        }
-        printf("\n");
-    }
-}
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = b * a;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			} else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+};
