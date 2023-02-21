10e1d068dc28df341f1935eccd88bc5f82b7deb1
akinola damilola <akinoladamilola1604@gmail.com>
2023-02-21 09:25:57 -0800

    reconcile

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
+}
